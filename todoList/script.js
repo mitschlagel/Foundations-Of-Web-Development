4d851605aa4e0a85ac29a769e62fe0378a9aa23a
Spencer Jones <spencerjones@creighton.edu>
2020-12-19 12:45:17 -0600

    Experiment with script.js

--- todoList/script.js
+++ todoList/script.js
@@ -1,6 +1,17 @@
 // Move task from text input to appropriate list
+    // If button clicked == true
+        // capture val in text 
+            // display val in appropriate list
+            // buttonClicked == flase
 
 // All options are required:
     // task name, list, and priority
     // lack of one will generate an error
 
+$(document).ready(function(){
+    // Get value on button click and show alert
+    $("#button").click(function(){
+        let task = $("#task").val();
+        console.log(task);
+    });
+});
