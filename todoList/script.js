bcd4e53cf754a384de5cc0c4defe3a74b59645b8
Spencer Jones <spencerjones@creighton.edu>
2020-12-21 16:27:53 -0600

    Implement remove item icon and experiment with functionality

--- todoList/script.js
+++ todoList/script.js
@@ -9,41 +9,35 @@
     // lack of one will generate an error
 
 $(document).ready(function(){
-    const home = $(".home");
-    const work = $(".work");
+    const home = $('.home');
+    const work = $('.work');
+    const removeButton = "<img class='removeButton' src='img/trash-can.png'>"
     
 
     // Task buttton functionality
-    $("button").click(function(){
-        let task = $(".task").val();
+    $('button').click(function(){
+        let task = $('.task').val();
         let priority = $(".priority[name='priority']:checked").val();
-        let list = $(".list").val();
-
+        let list = $('.list').val();
+        
         // Check for valid input
         if (task == undefined || priority == undefined || list == undefined) {
-            $(".error").empty().append("Task, list, and priority required.")
+            $('.error').empty().append('Task, list, and priority required.')
         } else {
+
             // Determine which list to add task and if it has tasks already
             if (list == "Home") {
-                $(".error").empty();
-                home.append("<p>" + task + " " + "<span>" + priority + "</span>" + "</p>");          
+                $('.error').empty();
+                home.append("<p class='taskItem'>" + task + " " + "<span>" + priority + "</span>" + removeButton + "</p>");          
             } else if (list == "Work") {
-                $(".error").empty();
-                work.append("<p>" + task + " " + "<span>" + priority + "</span>" + "</p>"); 
+                $('.error').empty();
+                work.append("<p class='taskItem'>" + task + " " + "<span>" + priority + "</span>" + removeButton + "</p>"); 
             }
         }
     });
     
-    // trash bin appears on hover, click to delete task
-    const taskItem = $(".home p");
-    const removeButton = "<img src='img/trash-can.png'>"
-    taskItem.mouseenter(function() {
-        taskItem.fadeIn(removeButton);
+    // click removeButton to remove taskItem (the parent element)
+    $('.removeButton').click(function(){
+        $(this).parent().remove();
     });
 });
-
-// Get value on button click and show alert
-// $("#button").click(function(){
-//     let task = $("#task").val();
-//     alert(task);
-// });
