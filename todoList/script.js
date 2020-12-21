9f3b15caf9188289f5c2e883a8fc8af2b5a5bc5a
Spencer Jones <spencerjones@creighton.edu>
2020-12-21 13:12:50 -0600

    Add priority span to task item and style it

--- todoList/script.js
+++ todoList/script.js
@@ -9,9 +9,11 @@
     // lack of one will generate an error
 
 $(document).ready(function(){
-    let home = $(".home");
-    let work = $(".work");
+    const home = $(".home");
+    const work = $(".work");
+    
 
+    // Task buttton functionality
     $("button").click(function(){
         let task = $(".task").val();
         let priority = $(".priority[name='priority']:checked").val();
@@ -19,32 +21,25 @@ $(document).ready(function(){
 
         // Check for valid input
         if (task == undefined || priority == undefined || list == undefined) {
-            $(".error").append("ERROR: task, list, and priority required.")
+            $(".error").empty().append("Task, list, and priority required.")
         } else {
-            // Determine which list to add task
+            // Determine which list to add task and if it has tasks already
             if (list == "Home") {
-                home.empty().append(task + " " + priority);
-                
+                $(".error").empty();
+                home.append("<p>" + task + " " + "<span>" + priority + "</span>" + "</p>");          
             } else if (list == "Work") {
-                work.empty().append(task + " " + priority);
+                $(".error").empty();
+                work.append("<p>" + task + " " + "<span>" + priority + "</span>" + "</p>"); 
             }
         }
-        
-        
-         
-        
-        
-        // if list == home
-            // .append(task) to home
-            // .append(priority)
-        // else if list == work
-            // .append(task) to work
-            // .append(prioriy)
-        // clear input
-        // reset list
-
     });
     
+    // trash bin appears on hover, click to delete task
+    const taskItem = $(".home p");
+    const removeButton = "<img src='img/trash-can.png'>"
+    taskItem.mouseenter(function() {
+        taskItem.fadeIn(removeButton);
+    });
 });
 
 // Get value on button click and show alert
