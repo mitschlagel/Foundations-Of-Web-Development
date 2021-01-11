2f0c698242ca7593d034ab454c8f23e10297cbc0
Spencer Jones <spencerjones@creighton.edu>
2021-01-11 14:35:43 -0600

    Add script for slideshow in main container

---
+++ Chipotle/js/slideshow.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".button-left").click(function() {
+        $(".main").css("background-image", "url(img/background.jpg)");
+    });
+    $(".button-right").click(function() {
+        $(".main").css("background-image", "url(img/background2.jpg)");
+    });
+
+});
