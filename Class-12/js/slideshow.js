33826d275a546a40860debff3919077aced5d2af
Spencer Jones <spencerjones@creighton.edu>
2021-01-21 20:08:38 -0600

    Redo portfolio slideshow

--- Class-12/js/slideshow.js
+++ Class-12/js/slideshow.js
@@ -1,37 +1,21 @@
- let slideIndex = 1;
- showSlides(slideIndex);
+$(document).ready(function() {
+    let i = 0;
+    const images = ["url(img/portfolio/1.jpg)", "url(img/portfolio/2.png)", 
+                        "url(img/portfolio/3.png)", "url(img/portfolio/4.png)"];
 
- // Next/prev controls
- function plusSlides(n) {
-     showSlides(slideIndex += n);
- }
+    
+    // Allow arrows on container to change bg image
+    $(".prev").click(function() {
+        if (i !== 0) {
+            i -= 1;
+        }
+        $(".current-slide").css("background-image", images[i]);  
+    });
+    $(".next").click(function() {
+        if (i !== images.length - 1) {
+            i += 1;
+        }
+        $(".current-slide").css("background-image", images[i]);
+    });
 
- // Thumbnail controls
- function currentSlide(n) {
-     showSlides(slideIndex = n);
- }
-
- function showSlides(n) {
-     let i;
-     let slides = document.getElementsByClassName("slide");
-     let dots = document.getElementsByClassName("demo");
-     let captionText = document.getElementById("caption");
-
-     if (n > slides.length) {
-         slideIndex = 1
-     } else if (n < 1) {
-         slideIndex = slides.length
-     }
-
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-
-     for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace(" active", "");
-     }
-
-     slides[slideIndex - 1].style.display = "block";
-     dots[slideIndex - 1].className += "active";
-     captionText.innerHTML = dots[slideIndex - 1].alt;
- } 
+});
