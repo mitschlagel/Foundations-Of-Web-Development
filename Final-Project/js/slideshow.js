75d062b0a6bf88d1d12422c62cbe9f4bc2762e3c
Spencer Jones <spencerjones@creighton.edu>
2021-01-24 21:14:51 -0600

    Work on portfolio slideshow style and script

---
+++ Final-Project/js/slideshow.js
@@ -0,0 +1,34 @@
+// Object for project urls
+const links = {
+    blanka: '#',
+    chipotle: '/Users/spencerjones/Desktop/code/aim/Foundations-Of-Web-Development/Chipotle/index.html',
+    dsc: '#',
+    wombat: '#',
+    todo: '#',
+    calc: '#'
+}
+
+$(document).ready(function() {
+    
+    // Change slide background-image when thumbnail is clicked
+    $('.thumbnails img').on('click', function() {
+        
+        // Var to hold selected item src
+        const item = $(this).prop('src');
+        const caption = $(this).prop('alt');
+        const id = $(this).prop('id');
+
+
+        // Change background image to item src
+        $('.current-slide').empty().css('background-image', 'url(' + item + ')');
+
+        // Change caption to image alt text
+        $('.caption').empty().append("<span>" + caption + "</span>");
+
+        // Wrap slide in a tag with link to project
+        $('.current-slide').wrap("<a href='" + links[id] + "' target='blank'></a>");
+
+
+    });
+});
+
