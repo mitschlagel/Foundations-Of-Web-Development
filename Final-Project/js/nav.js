d3e7502c94acf3e44fa4ead588c199a94aa91b6b
Spencer Jones <spencerjones@creighton.edu>
2021-01-25 22:16:39 -0600

    Start work on mobile.css, create mobile banner and nav

--- Final-Project/js/nav.js
+++ Final-Project/js/nav.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 
-    // Fades secondary nav into view on scroll
-    $(window).scroll(function() {
-        let scroll = $(window).scrollTop();
 
-        if (scroll >= 500) {
-            $(".small-nav").addClass("is-visible");
-        } else {
-            $(".small-nav").removeClass("is-visible");
-        }
-    });
+    // Check for small screens 
+    if ($(window).width <= 600) {
+        
+        $(".small-nav").removeClass("is-visible");
+        
+        
+    } else {
+        // Fades secondary nav into view on scroll
+        $(window).scroll(function() {
+            let scroll = $(window).scrollTop();
+
+            if (scroll >= 500) {
+                $(".small-nav").addClass("is-visible");
+            } else {
+                $(".small-nav").removeClass("is-visible");
+            }
+        });
+    }
+    
+        
 
     // Animates a slow scroll to each section when clicked on the nav
     $("a[href='#home']").click(function() {
