368363c313f8c64484d7c4f3ec57c0af4821ff03
Spencer Jones <spencerjones@creighton.edu>
2021-02-11 19:39:10 -0600

    Mid class assesment progress 2-11-21

--- assesment/js/orange.js
+++ assesment/js/orange.js
@@ -2,23 +2,37 @@ $(document).ready(function() {
     
     // Functions to move pawprint around <nav>
     
-    $("a[href='#home']").hover(function () {
-            // over
-            $('#pawprint').addClass('pawprint-home');
-        }, function () {
-            // out
-            $('#pawprint').removeClass('pawprint-home');
-        });
     
 
-    // Functions to display content on nav clicks
+    // Functions to display content on nav clicks.
     $("a[href='#schedule']").click(function () {   
-        $('.schedule').addClass("is-visible"); 
+        $('.schedule').addClass("sched-is-visible"); 
+    });
+
+    $('#sched-close-button').click(function() {
+        $(':parent').removeClass("sched-is-visible");
     });
     
+    $("a[href='#contact']").click(function () {   
+        $('.contact').addClass("contact-is-visible"); 
+    });
     
-    $('#close-button').click(function() {
-        $(':parent').removeClass("is-visible");
+    // Button to close sched and contact windows.
+    $('#contact-close-button').click(function() {
+        $(':parent').removeClass("contact-is-visible");
+    });
+
+    // Functions for scrolling between Home and About pages.
+    $("a[href='#home']").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".home").offset().top
+        }, 400);
+    });
+
+    $("a[href='#about']").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".about").offset().top
+        }, 400);
     });
 });
 
